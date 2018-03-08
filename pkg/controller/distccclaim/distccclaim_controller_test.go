58a36728575328d326bf40b50cbb2099b7d5a207
Michele Bertasi <michele.bertasi@brightcomputing.com>
2018-03-08 16:08:14 +0100

    add DistccClaim controller test for expiration

--- pkg/controller/distccclaim/distccclaim_controller_test.go
+++ pkg/controller/distccclaim/distccclaim_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	kubeerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	fakekubeclient "k8s.io/client-go/kubernetes/fake"
@@ -28,6 +29,15 @@ func (c *controllerTest) Start(stopCh <-chan struct{}) {
 
 func (c *controllerTest) Now() time.Time {
 	return c.now
+}
+
+// Sync calls the controller Sync with passing the most up to date version of the resource
+func (c *controllerTest) Sync(namespace, name string) (bool, error) {
+	obj, err := c.k8ccclientset.K8ccV1alpha1().DistccClaims(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		return false, err
+	}
+	return c.controller.Sync(obj)
 }
 
 func newController(objects ...runtime.Object) *controllerTest {
@@ -58,6 +68,7 @@ func TestDistccRefCheck(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "foobar",
 			Namespace: "default",
+			Labels:    map[string]string{"track": "stable"},
 		},
 		Spec: k8ccv1alpha1.DistccClaimSpec{
 			DistccName: "",
@@ -67,6 +78,7 @@ func TestDistccRefCheck(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "dev",
 			Namespace: "default",
+			Labels:    map[string]string{"track": "unstable"},
 		},
 	}
 	ctrl := newController(claim, distcc)
@@ -77,7 +89,7 @@ func TestDistccRefCheck(t *testing.T) {
 	defer close(stopCh)
 
 	// Sync a claim without a distcc name fails
-	sync, err := ctrl.Sync(claim)
+	sync, err := ctrl.Sync("default", "foobar")
 	assert.False(t, sync)
 	assert.NotNil(t, err)
 	event, ok := kit.HasEvent(err)
@@ -90,7 +102,20 @@ func TestDistccRefCheck(t *testing.T) {
 	assert.Nil(t, err)
 
 	// Sync a claim with a wrong distcc ref fails
-	sync, err = ctrl.Sync(claim)
+	sync, err = ctrl.Sync("default", "foobar")
+	assert.False(t, sync)
+	assert.NotNil(t, err)
+	event, ok = kit.HasEvent(err)
+	assert.True(t, ok)
+	assert.Equal(t, event.Reason, ErrDistccNotFound)
+
+	// Update the ref to use the existing "dev" distcc
+	claim.Spec.DistccName = "dev"
+	_, err = ctrl.k8ccclientset.K8ccV1alpha1().DistccClaims("default").Update(claim)
+	assert.Nil(t, err)
+
+	// Sync a claim with different labels fails
+	sync, err = ctrl.Sync("default", "foobar")
 	assert.False(t, sync)
 	assert.NotNil(t, err)
 	event, ok = kit.HasEvent(err)
@@ -135,7 +160,7 @@ func TestDistccSetExpiration(t *testing.T) {
 	assert.Nil(t, err)
 
 	// The Sync should succeed now
-	sync, err := ctrl.Sync(claim)
+	sync, err := ctrl.Sync("default", "mike-master")
 	assert.Nil(t, err)
 	assert.True(t, sync)
 
@@ -144,4 +169,67 @@ func TestDistccSetExpiration(t *testing.T) {
 	expectedExp := ctrl.now.Add(10 * time.Minute).Truncate(time.Second)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedExp, claim.Status.ExpirationTime.Time)
+}
+
+func TestDistccDeleteWhenExpired(t *testing.T) {
+	labels := map[string]string{
+		"track": "stable",
+	}
+	claim := &k8ccv1alpha1.DistccClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mike-master",
+			Namespace: "default",
+			Labels:    labels,
+		},
+		Spec: k8ccv1alpha1.DistccClaimSpec{
+			DistccName: "master",
+		},
+	}
+	distcc := &k8ccv1alpha1.Distcc{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "master",
+			Namespace: "default",
+			Labels:    labels,
+		},
+		Spec: k8ccv1alpha1.DistccSpec{
+			LeaseDuration: metav1.Duration{Duration: 10 * time.Minute},
+		},
+	}
+	ctrl := newController(claim, distcc)
+
+	// Start the controller
+	stopCh := make(chan struct{})
+	ctrl.Start(stopCh)
+	defer close(stopCh)
+
+	// Sync to set the expiration
+	sync, err := ctrl.Sync("default", "mike-master")
+	assert.Nil(t, err)
+	assert.True(t, sync)
+
+	// Check that the claim is still there
+	_, err = ctrl.k8ccclientset.K8ccV1alpha1().DistccClaims("default").Get("mike-master", metav1.GetOptions{})
+	assert.Nil(t, err)
+
+	// Some time passes, but not enough for the expiration
+	ctrl.now = ctrl.now.Add(6 * time.Minute)
+	assert.Equal(t, ctrl.now, ctrl.controller.now())
+
+	sync, err = ctrl.Sync("default", "mike-master")
+	assert.Nil(t, err)
+	assert.False(t, sync)
+
+	// Check that the claim is still there
+	_, err = ctrl.k8ccclientset.K8ccV1alpha1().DistccClaims("default").Get("mike-master", metav1.GetOptions{})
+	assert.Nil(t, err)
+
+	// Then we have an expiration
+	ctrl.now = ctrl.now.Add(5 * time.Minute)
+	sync, err = ctrl.Sync("default", "mike-master")
+	assert.Nil(t, err)
+	assert.False(t, sync)
+
+	// Check that the claim is gone
+	_, err = ctrl.k8ccclientset.K8ccV1alpha1().DistccClaims("default").Get("mike-master", metav1.GetOptions{})
+	assert.True(t, kubeerr.IsNotFound(err))
 }
